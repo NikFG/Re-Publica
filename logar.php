7367c775bf9989e5c905f0c9e9e134896325302a
PedroLTS <pedro.silva520@etec.sp.gov.br>
2018-06-20 19:52:46 -0300

    LOGIN PORRA
    AGORA O LOGIN ESTA FUNFANDO E SO OQ FALTA E O CADASTRO DE REPUBLICAS, BJO BJO BJAO

--- logar.php
+++ logar.php
@@ -8,36 +8,45 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-
     </head>
     <body>
         <?php
-        $servidor = "localhost";
-        $user = "root";
-        $senha = "";
-        $banco = "dbrepublica";
+         $servidor = "localhost";
+       $user = "root";
+       $senha = "";
+       $banco = "dbrepublica";
+       $nome;
+       
+       
         try {
             $conn = new PDO("mysql:host=$servidor;dbname=$banco", $user, $senha);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "insert into tbpessoa(nome,senha,cpf,email) VALUES ( '" . $_POST["nome"] .
-                    "', '" . $_POST["senha"] .
-                    "', '" . $_POST["cpf"] .
-                    "', '" . $_POST["email"] . "')";
+            $sql ="select nome from tbpessoa where nome = '".$_POST["login"]."' and senha = '".$_POST["senha"]."'";
             $stmt = $conn->prepare($sql);
             $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_NUM);
+                while ($row = $stmt->fetch()) {
+                    $nome = $row[0];
+                  
+                }
+            
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         } finally {
             $conn = null;
            
         }
-        ?>
-    </body>
-    <script>
+        echo "Usuario conectado:". $nome;
+      
+      
+      ?>
+        <script>
         
         
-        window.setTimeout("location.href='index.html'",500);
+        window.setTimeout("location.href='index.html'",10000);
  
 
     </script>
+       
+    </body>
 </html>
